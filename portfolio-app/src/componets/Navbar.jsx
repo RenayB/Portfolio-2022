e84e2dd14005a6d8f10630c84867c9e4062abe19
RenayB <nayrb1236@gmail.com>
2022-07-09 12:34:22 -0500

    Update Navbar.jsx
    Possible "image not found" fix

--- portfolio-app/src/componets/Navbar.jsx
+++ portfolio-app/src/componets/Navbar.jsx
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import {FaBars, FaTimes, FaGithub,FaLinkedin} from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../assets/R.png';
+import Logo from './Portfolio-2022/assets/R.png';
 import { Link } from 'react-scroll/modules';
 
 const Navbar = () => {
